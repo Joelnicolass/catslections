9d298367a93116a480e82ffd6b90f885cdcfeb19
Sartori Joel Nicolas <joelnicolass@gmail.com>
2023-03-19 19:13:11 -0300

    add page

--- src/pages/cats/views/RateRandomCatView.jsx
+++ src/pages/cats/views/RateRandomCatView.jsx
@@ -23,7 +23,44 @@ const RateRandomCatView = () => {
   const sendToServer = (id, rate) => {};
 
   return (
-    <div>
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-around",
+        alignItems: "center",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <h1
+          style={{
+            fontSize: "2.5rem",
+            fontWeight: "bold",
+            color: "#000",
+          }}
+        >
+          Rate a random cat
+        </h1>
+        <h6>
+          <i
+            style={{
+              fontWeight: "bold",
+              color: "#000",
+            }}
+          >
+            Click on the card to vote
+          </i>
+        </h6>
+      </div>
+
       <VisorRate
         card={{
           id: urlImageCat,
