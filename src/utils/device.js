8c5fcf07c5a71cb8d36fedf22d49ea4bca00fb19
Sartori Joel Nicolas <joelnicolass@gmail.com>
2023-03-19 15:23:05 -0300

    [hotfix]: change color button | add device detect

--- src/utils/device.js
+++ src/utils/device.js
@@ -1,8 +1,8 @@
 export const isMobile = () => {
   try {
     if (typeof window.orientation !== "undefined") return true;
+    if (window.screen.width < 768) return true;
     //if (navigator.userAgent.indexOf("IEMobile") !== -1) return true;
-    //if (window.screen.width < 768) return true;
     //if (navigator.maxTouchPoints > 5) return true;
 
     return false;
